c65558066acd025bb6ae56cc7a0bfa9d1de8beb8
karan <yadavkaran471@gmail.com>
2024-05-27 19:01:08 +0530

    master crud

--- Models/Bills.models.js
+++ Models/Bills.models.js
@@ -2,26 +2,8 @@ import mongoose from "mongoose";
 
 const billSchema = new mongoose.Schema(
   {
-    Particular: {
-      type: String,
-    },
-    Amount: {
-      type: String,
-    },
-    Rate: {
-      type: String,
-    },
-    From: {
-      type: String,
-    },
-    To: {
-      type: String,
-    },
-    DueDate: {
-      type: String,
-    },
-    BillNo: {
-      type: String,
+    data: {
+      type: Object,
     },
   },
   {
--- controllers/society.controller.js
+++ controllers/society.controller.js
@@ -3,18 +3,11 @@ import Bills from "../Models/Bills.models.js";
 export const generateBills = async (req, res) => {
   console.log("reached inside gerenateBills controllers", req.body);
   try {
-    req.body.map((item) => {
-      let result = new Bills({
-        Particular: item.particulars[0],
-        Amount: item.amnt.value,
-        Rate: item.rate.value,
-        From: item.dateFieldValues.fromDate,
-        To: item.dateFieldValues.toDate,
-        DueDate: item.dateFieldValues.dueDate,
-        BillNo: item.BillNo,
-      });
-      result.save();
+    let result = new Bills({
+      data: req.body,
     });
+    await result.save();
+
     res.send("successfully sent data to database");
   } catch (error) {
     res.send(error);
